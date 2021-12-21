b4e84527bf9aeea750ea3971329e71481304c93d
Felipe <jflobom@gmail.com>
2021-12-21 15:59:30 -0500

    Add post model

--- src/db/init.ts
+++ src/db/init.ts
@@ -1,4 +1,5 @@
 import User from './models/User'
+import Post from './models/Post'
 
 require('dotenv').config({ path: '.env' })
 
@@ -6,5 +7,6 @@ const isDev = process.env.NODE_ENV === 'development'
 
 const dbInit = async () => {
   await User.sync({ alter: isDev })
+  await Post.sync({ alter: isDev })
 }
 export default dbInit
---
+++ src/db/models/Post.ts
@@ -0,0 +1,66 @@
+import { DataTypes, Model, Optional } from 'sequelize'
+import sequelizeConnection from '../config'
+
+interface PostAttributes {
+  id: number
+  userId: number
+  content: string
+  createdAt?: Date
+  updatedAt?: Date
+}
+
+export interface PostInput
+  extends Optional<PostAttributes, 'content' | 'userId'> {}
+
+export interface PostOutput extends Required<PostAttributes> {}
+
+class Post extends Model<PostAttributes, PostInput> implements PostAttributes {
+  public id!: number
+
+  public content!: string
+
+  public userId!: number
+
+  public readonly createdAt!: Date
+
+  public readonly updatedAt!: Date
+}
+
+Post.init(
+  {
+    id: {
+      type: DataTypes.INTEGER.UNSIGNED,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    userId: {
+      type: DataTypes.INTEGER.UNSIGNED,
+      allowNull: false,
+      field: 'user_id',
+      references: {
+        key: 'id',
+        model: 'users',
+      },
+    },
+
+    content: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      field: 'created_at',
+    },
+    updatedAt: {
+      type: DataTypes.DATE,
+      field: 'updated_at',
+    },
+  },
+  {
+    timestamps: true,
+    sequelize: sequelizeConnection,
+    tableName: 'posts',
+  }
+)
+
+export default Post
