6cc8f7a1f00751175a61b6c318fb909eb4e9ffdd
Felipe <jflobom@gmail.com>
2021-12-21 16:04:55 -0500

    Add post likes & comments models

--- src/db/init.ts
+++ src/db/init.ts
@@ -1,5 +1,7 @@
 import User from './models/User'
 import Post from './models/Post'
+import PostComment from './models/PostComment'
+import PostLike from './models/PostLike'
 
 require('dotenv').config({ path: '.env' })
 
@@ -8,5 +10,7 @@ const isDev = process.env.NODE_ENV === 'development'
 const dbInit = async () => {
   await User.sync({ alter: isDev })
   await Post.sync({ alter: isDev })
+  await PostComment.sync({ alter: isDev })
+  await PostLike.sync({ alter: isDev })
 }
 export default dbInit
